c306111612158cc74a5dd32f8eee514fc16c8ef6
Piyush Sehgal <>
2021-02-03 17:28:03 +0530

    Feb 02, 2021 Trim BST

---
+++ src/main/java/com/leetcode/monthly/_2021/feb/_2/TrimBST.java
@@ -0,0 +1,38 @@
+package com.leetcode.monthly._2021.feb._2;
+
+public class TrimBST {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return root;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
